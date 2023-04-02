5227a91681c5d9b72af0567759519b4f0e50fe81
SamVa <sv.samuel.valencia014@gmail.com>
2023-04-02 14:04:13 -0400

    start reworking parser

---
+++ Parser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VRCGame
+{
+    public class Parser
+    {
+        public void LoadFile(string path)
+        {
+
+        }
+    }
+}
