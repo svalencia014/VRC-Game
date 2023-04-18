39561ff34cbaa4a1d02b45cceab3f92a97af8f91
michaelcmelton <michaelcmelton93@gmail.com>
2023-04-17 21:10:10 -0400

    refactor: Move Models into separate folder

--- Program.cs
+++ Program.cs
@@ -2,13 +2,12 @@
 {
   public class Program
   {
-    public static FSDServer fsdServer;
+    public static FSDServer? fsdServer;
     public static void Main()
     {
-      string? airportPath;
       Console.WriteLine("VRC Game v0.0.1");
       Console.WriteLine("Please Enter the Path of your Facility (.json) file");
-      airportPath = Console.ReadLine();
+      string? airportPath = Console.ReadLine();
       //Console.WriteLine("Enter the Path of your Situation (.sit) file or press enter to skip");
       //path = Console.ReadLine();
       //if (path != null)
